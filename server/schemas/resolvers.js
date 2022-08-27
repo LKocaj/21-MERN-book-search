9f9e36ea6308087d2971903628881f41099b2c1c
Lawrence <lawrencecaj@gmail.com>
2022-08-27 18:16:44 -0400

    schema perhaps

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,17 +3,17 @@ const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models");
 const { signToken } = require("../utils/auth");
 
-Query: {
-    me: async (parent, args, context) => {
-        if (context.user) {
-            const userData = await User.findOne({ _id: context.user._id }).select(
-              "-__v -password"
-            );
-            return userData;
-          }
-          throw new AuthenticationError("Not logged in");
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
         }
-      },
+    },
       Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
